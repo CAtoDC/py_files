ece6b196a9ac1abe7d3b6d966ad3801184fe74fb
CAtoDC <mark.cuevas@gmail.com>
2016-01-18 08:55:39 -0500

    v01

---
+++ Class_Pet.py
@@ -0,0 +1,23 @@
+class Pet(object):
+	'Common base class for all pets'
+	petCount = 0
+
+	def __init__(self, name, species):
+		self.name = name
+		self.species = species
+		Pet.petCount += 1	# Increment for each pet at creation-time
+
+	def getName(self):
+		return self.name
+
+	def getSpecies(self):
+		return self.species
+
+	def __str__(self):
+		return "%s is a %s." % (self.name, self.species)
+
+pet01 = Pet("Polly", "Parrot")
+pet02 = Pet("Noah", "Dog")
+
+print pet01
+print "I have %d pets." % Pet.petCount
