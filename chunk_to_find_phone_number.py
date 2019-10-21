bc538ea789935dc42153d66fffe74f5a653bae3f
Mark Cuevas <mark.cuevas@gmail.com>
2019-10-20 21:46:04 -0400

    Added chunk and removed pipfile

---
+++ chunk_to_find_phone_number.py
@@ -0,0 +1,32 @@
+'''From: Automate the Boring Stuff with Python
+
+Find a phone number in a string using chunk.
+
+This assumes a phone number pattern of 202-555-1212
+and is therefore very limited (though it is quick)'''
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+message = 'Call me at 202-555-1011 tomorrow. 202-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: ' + chunk)
+print('Done')
+
